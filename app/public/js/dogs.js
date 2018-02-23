e781d2a241ba6f3b2202405ff40c71b06b6520b9
eogiesoba <eogiesoba@gmail.com>
2018-02-23 02:02:21 -0600

    Updated Backend logic to find match dog

--- app/public/js/dogs.js
+++ app/public/js/dogs.js
@@ -1,13 +1,45 @@
 //Frond-End logic for the Dog survey to take in user inputs and send to DB and output a final result
 
-$(document).ready(function(){ //This ensures DOM elements are loaded
+$(document).ready(function () { //This ensures DOM elements are loaded
     // When user clicks add-btn
-    $("#add-btn").on("click", function(event) {
+    $("#add-btn").on("click", function (event) {
         // event.preventDefault();
         console.log("you are about to enter the api route get call");
+
+        // Creates a new survey object
+        var newSurvey = { 
+            Q1: parseInt($("#question1").val()),  
+            Q2: parseInt($("#question2").val()),  
+            Q3: parseInt($("#question3").val()),  
+            Q4: parseInt($("#question4").val()),  
+            Q5: parseInt($("#question5").val()),  
+            Q6: parseInt($("#question6").val()),  
+            Q7: parseInt($("#question7").val()),   
+            Q8: parseInt($("#question8").val()),  
+            Q9: parseInt($("#question9").val()),  
+            Q10: parseInt($("#question10").val()),
+            name: $("#name").val().trim(),
+            link: $("#link").val().trim()  
+        };
+
+        //Validate Survey
+        var pass = true;
+        (function surveyCheck(){
+            for (var property in newSurvey) {
+                if (newSurvey[property]){}
+                else{   
+                    pass = false;
+                }
+            }
+        })();
+       console.log(pass);
         //An AJAX request wit jQuery. This finds the matching dog row to be used here on the front-end
-        $.get("/api/match", function(data) {
-            console.log(data);
-        });
+        if (pass) {
+            $.post("/api/match", newSurvey, function (data) {
+                console.log("New Survey: ", newSurvey);
+            });
+        }else{
+            alert("Please fill in all fields in order to complete survey");
+        }
     });
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,10 +11,42 @@ var Dogs = require("../models/table.js");
 module.exports = function(app) {
 
   // Get DB info and find matching dog, then send dog info to the front-end. 
-  app.get("/api/match", function(req, res) {
+  app.post("/api/match", function(req, res) {
     Dogs.findAll({}).then(results => {
-      res.json(results);
-      console.log(JSON.stringify(results));
+      var surveyArr = [];
+      var tableArr = [];
+      var surveyData = req.body;//Object
+      var tableData = JSON.parse(JSON.stringify(results));//Array of Objects
+
+      for (var k in surveyData) {//Push survey reponse data from object to an array
+        surveyArr.push(surveyData[k]);
+      }
+      for(var i=0; i < tableData.length; i++){//Push all table values onto into an array of arrays => matrix. 
+        miniArr = [];
+        for(var k in tableData[i]){
+          miniArr.push(tableData[i][k]);
+        }
+        miniArr.shift();
+        tableArr.push(miniArr);
+      }
+
+      totalDiff = [];
+
+      for (var i=0; i < 10; i++){
+          var diff = 0;
+          for (var a=0; a < 10; a++){
+            diff += Math.abs(tableArr[i][a] - surveyArr[a]);
+          }
+          totalDiff.push(diff);
+      }
+      var index = totalDiff.indexOf(Math.min(...totalDiff));
+      var id = index + 1;
+
+      console.log(surveyArr);
+      console.log(tableArr);
+      console.log(totalDiff);
+      console.log(index);
+      res.end();
     });
   });
 };
