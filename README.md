312410e211721bca0db0ea4a955012333769f0b5
eogiesoba <eogiesoba@gmail.com>
2018-04-12 00:56:27 -0500

    Added picture to survey & updated ReadME

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# DogFinder_rep
-Dog Finder App.
+# Dog Finder
+
+<https://efosa-dog-finder.herokuapp.com/> \
+Version 1.0 \
+Deployment Date: February 19, 2018  \
+Contributors: Efosa Ogiesoba
+
+# Purpose
+
+This is a simple app that will help users find their dream dog. It was built as a homework project to help increase proficiency in Express.js and Anime.js. Users will take a short survey and app will display their dog.
+
+# Screenshot - 1 - Dog Finder Home Page
+
+![Dog Finder Home Page](app/public/assets/images/dogFinder.png)
+
+# Screenshot - 2 - Dog Finder Survey Page
+
+![Dog Finder Survey Page](app/public/assets/images/dogFinder.png)
+
+# IceBox
+
+Ideas for future development.
+
+1) Add link to nearby animal shelters where you can find your dream dog.
--- app/public/js/dogs.js
+++ app/public/js/dogs.js
@@ -1,31 +1,35 @@
 //Frond-End logic for the Dog survey to take in user inputs and send to DB and output a final result
 
 $(document).ready(function () { //This ensures DOM elements are loaded
+
+    $("#closeModal").on("click", function (event) {
+        $('#exampleModal').modal('hide');
+    })
+
     // When user clicks add-btn
     $("#add-btn").on("click", function (event) {
         // event.preventDefault();
         // Creates a new survey object
-        var newSurvey = { 
-            Q1: parseInt($("#question1").val()),  
-            Q2: parseInt($("#question2").val()),  
-            Q3: parseInt($("#question3").val()),  
-            Q4: parseInt($("#question4").val()),  
-            Q5: parseInt($("#question5").val()),  
-            Q6: parseInt($("#question6").val()),  
-            Q7: parseInt($("#question7").val()),   
-            Q8: parseInt($("#question8").val()),  
-            Q9: parseInt($("#question9").val()),  
+        var newSurvey = {
+            Q1: parseInt($("#question1").val()),
+            Q2: parseInt($("#question2").val()),
+            Q3: parseInt($("#question3").val()),
+            Q4: parseInt($("#question4").val()),
+            Q5: parseInt($("#question5").val()),
+            Q6: parseInt($("#question6").val()),
+            Q7: parseInt($("#question7").val()),
+            Q8: parseInt($("#question8").val()),
+            Q9: parseInt($("#question9").val()),
             Q10: parseInt($("#question10").val()),
-            name: $("#name").val().trim(),
-            link: $("#link").val().trim()  
+            name: $("#name").val().trim()
         };
 
         //Validate Survey
         var pass = true;
-        (function surveyCheck(){
+        (function surveyCheck() {
             for (var property in newSurvey) {
-                if (newSurvey[property]){}
-                else{   
+                if (newSurvey[property]) { }
+                else {
                     pass = false;
                 }
             }
@@ -34,10 +38,25 @@ $(document).ready(function () { //This ensures DOM elements are loaded
         //An AJAX request wit jQuery. This finds the matching dog row to be used here on the front-end
         if (pass) {
             $.post("/match", newSurvey, function (data) {
-                alert("Matching Dog: " + data[10] + "\n Matching Link: " + data[11]);
+                $(".modal-title").html("Say hello to your dream dog!");
+                $("#dog-name").show().html(data[10]);
+                $("#imageDog").show().attr("src", data[11]);
+                $('#exampleModal').modal({
+                    show: true,
+                    keyboard: false,
+                    backdrop: 'static'
+                });
             });
-        }else{
-            alert("Please fill in all fields in order to complete survey");
+        } 
+        else {
+            $(".modal-title").html("Please fill in all fields in order to complete survey");
+            $("#imageDog").hide();
+            $("#dog-name").hide();
+            $('#exampleModal').modal({
+                show: true,
+                keyboard: false,
+                backdrop: 'static'
+            });
         }
     });
 });
